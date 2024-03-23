5bf8d0dc6dded27622e9a82e01bcba901c2d9540
fadegor05 <fad.egor05@gmail.com>
2024-03-23 09:10:23 +0400

    добавлены crud функции для локаций

---
+++ app/crud/location.py
@@ -0,0 +1,20 @@
+import datetime
+from sqlalchemy import Select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import Location, Travel
+
+
+async def get_location_by_id(session: AsyncSession, id: int) -> Location | None:
+    stmt = Select(Location).where(Location.id == id)
+    location = await session.scalar(stmt)
+    return location
+
+
+async def create_location(session: AsyncSession, travel: Travel, city: str, country: str, arrive_at: datetime.datetime,
+                          departure_at: datetime.datetime) -> Location:
+    location = Location(travel=travel, city=city, country=country, arrive_at=arrive_at, departure_at=departure_at)
+    session.add(location)
+    await session.commit()
+    await session.refresh(location)
+    return location
