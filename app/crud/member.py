1d2655be7cfb036d44a8b6000d8a5b553158570b
fadegor05 <fad.egor05@gmail.com>
2024-03-24 11:17:53 +0400

    добавлено создание и удаление участников в crud

--- app/crud/member.py
+++ app/crud/member.py
@@ -1,7 +1,20 @@
 from sqlalchemy import Select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.models import Travel, Member
+from app.models import Travel, Member, User
+
+
+async def create_member(session: AsyncSession, travel: Travel, user: User, is_owner: bool = False) -> Member:
+    member = Member(user=user, travel=travel, is_owner=is_owner)
+    session.add(member)
+    await session.commit()
+    await session.refresh(member)
+    return member
+
+
+async def delete_member(session: AsyncSession, member: Member) -> None:
+    await session.delete(member)
+    await session.commit()
 
 
 async def get_member_by_id(session: AsyncSession, id: int) -> Member | None:
