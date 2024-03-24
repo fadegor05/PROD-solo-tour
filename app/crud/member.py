84b32770934850133f7fe56d0a73d081be1dc6b7
fadegor05 <fad.egor05@gmail.com>
2024-03-24 10:22:41 +0400

    добавлено получения участника по айди в crud

--- app/crud/member.py
+++ app/crud/member.py
@@ -1,6 +1,13 @@
+from sqlalchemy import Select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.models import Travel
+from app.models import Travel, Member
+
+
+async def get_member_by_id(session: AsyncSession, id: int) -> Member | None:
+    stmt = Select(Member).where(Member.id == id)
+    member = await session.scalar(stmt)
+    return member
 
 
 async def delete_members_by_travel(session: AsyncSession, travel: Travel) -> None:
